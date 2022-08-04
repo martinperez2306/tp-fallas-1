4f2381bc9e1b27e57e8b8efb07713875a97aee0c
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-08-04 02:00:17 -0300

    Rename and move calls

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from typing import Union
 from pydantic import BaseModel
+from medical_expert import *
 
 class Report(BaseModel):
     symptom: str
@@ -18,4 +19,8 @@ def read_root():
 
 @app.post("/diagnostics")
 def read_item(report: Report):
+    engine = MedicalRobot()
+    engine.reset()
+    engine.declare(ReportModel(tos='TS'), ReportModel(congestion='MA'), ReportModel(fiebre='F'))
+    engine.run()
     return report
